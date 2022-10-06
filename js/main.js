f692d7113921985a785a46d5b054f594b095af2d
yamaneko-algorithm <110741591+yamaneko-algorithm@users.noreply.github.com>
2022-10-07 03:16:22 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -545,6 +545,15 @@ function around(state) {
 
 document.documentElement.style.setProperty('--width', `${window.innerWidth}`);
 
+/* 更新日の取得 */
+const last = new Date(document.lastModified);
+const year = last.getFullYear();
+const month = last.getMonth() + 1;
+const date = last.getDate();
+/* 日付を書き換える */
+const target = document.getElementById('modify');
+target.textContent = year + '-' + month + '-' + date;
+
 btn();
 createBoard();
 showInitialState(initialState);
